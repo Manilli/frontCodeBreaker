9774d479549dabf7e45d0279ed96f5c8440cf055
sonorkz <osorno.sebastian@gmail.com>
2018-06-02 12:38:12 -0500

    Front completo, consume servicios

--- codebreakerfront/src/App.js
+++ codebreakerfront/src/App.js
@@ -1,54 +1,92 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
 
 class App extends Component {
-  
+
   constructor() {
     super();
-    this.state = { items: [] };
+    this.state = 
+      { items: [],
+        person: [],
+        valor: "",
+        respuesta :"",
+        clave:"",
+        defSecreto:"",
+        advSecreto:""
+
+      };
+    this.controlCambiosSet = this.controlCambiosSet.bind(this);
+    this.controlCambiosDef = this.controlCambiosDef.bind(this);
+    
+    this.adivinar = this.adivinar.bind(this);
+    this.enviar = this.enviar.bind(this);
 }
 
-componentDidMount() {
-  fetch(`https://codebreaker-api-jd-s.herokuapp.com/adivinarsecreto/:1598`)
-  .then(result=>result.json())
-  .then(items=>this.setState({items}))
-    /*fetch(`https://codebreaker-api-jd-s.herokuapp.com/adivinarsecreto/:1598`) 
-        .then(result=> {
-            this.setState({items:result.json()});
-        });*/
+adivinar() {
+  var cadena;
+  this.cadena = this.state.advSecreto;
+  axios.get('http://localhost:5000/adivinarsecreto/'+ this.cadena)
+  .then(res=>  {
+    const respuesta = res.data;
+    this.setState({respuesta});
+  })
 }
+
+enviar() {
+  var cadena;
+  this.cadena = this.state.defSecreto;
+
+  axios.post('http://localhost:5000/setsecreto/',{ "secret": this.cadena })
+  .then(res=>  {
+    const clave = res.data;
+    this.setState({clave});
+  })
+}
+
+controlCambiosSet(event) {
+  this.setState({advSecreto: event.target.value});
+}
+
+controlCambiosDef(event) {
+  this.setState({defSecreto: event.target.value});
+}
+
 
 render() {
   
   return (
-    <div>
-    <h1>Juan David y Sebastián - CodeBreaker</h1>
-    <input type="text" placeholder="code"></input>
-    <div>
-      <button onClick={this.props.resetearPartida}>Adivinar</button>
-    </div>
+    <div className="App">
+    <header className="App-header">
+
+          <h1 className="App-title">Juan David y Sebastián - CodeBreaker</h1>
+        </header>
+    <div className="App-intro">
+      <div className="definir">
+      <p>Definir clave </p>
+      <input type="text" placeholder="Enviar Secreto"  onChange={this.controlCambiosDef} />
+      <button type="submit" value="Submit" onClick={this.enviar}> Enviar </button>
+        <p>Clave definida: {this.state.clave}</p>
+      </div>
+      <div className="adivinar">
+      <p>Adivinar: </p>
+      <input type="text" placeholder="Adivinar Secreto" onChange={this.controlCambiosSet} />
+      <button type="submit" value="Submit" onClick={this.adivinar}> Adivinar </button>
+        
+          <p>Respuesta: {this.state.respuesta}</p>
+        </div>
     
+    </div>
+
     </div>
   );
-}
-  
-  
-  
+} 
 }
 
-/*       
-    return(
-        <div>
-            <div>Items:</div>
-            <ul>
-              {this.state.items.map(item=><li key={item.id}>{item.body}</li>)}
-            </ul>          
-        </div>  
-    );*/
 
-  /*
-  render() {
-    
-  */
 export default App;
+
+
